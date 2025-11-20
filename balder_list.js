6d9c33f6a654d9b56e82980eb7c0bfc738091599
skannea <anders@lannea.se>
2025-11-20 19:22:40 +0100

    åäö

--- balder_list.js
+++ balder_list.js
@@ -1,8 +1,9 @@
-//
+// only text files are allowed here
 filelist = [
-    { file: "balder_angels_app.py",     desc: "Apllikation för att testa orientation"},
+    { file: "balder_angels_app.py",     desc: "Applikation för att testa orientation"},
     { file: "balder_com.py",            desc: "Kommunikationsmodul för Balder" },
-    { file: "balder.js",                desc: "JavaScript klient för Balder" },
-    { file: "balder_list.js",         desc: "Denna fil med fillista" },
+    { file: "balder.js",                desc: "JavaScript-klient för Balder" },
+    { file: "balder.css",                desc: "CSS-stilar räksmörgås RÄKSMÖRGÅS" },
+    { file: "balder_list.js",           desc: "Denna fil med fillista" },
 ]    
     
